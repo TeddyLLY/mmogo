2285db5e0fb8812f6c6e686f58c9ab031aaa298a
teddy <zzx94x94@gmail.com>
2024-06-12 19:36:16 +0800

    update memo

--- src/main/.script/mongo/data/mongosh.sh
+++ src/main/.script/mongo/data/mongosh.sh
@@ -1,5 +1,5 @@
 ### 塞大量測試資料 ###
-# insert one
+# insertOne
  for (var i = 1; i <= 10000; i++){
       db.employee.insertOne([
            {
@@ -15,15 +15,17 @@
    };
 
 
+# insertMany
+# 注意 insertMany 超過 1000 筆會分拆多個小組分次 insert db
+# push 一個 json array 十萬左右可以接受 , 超過 mongo shell 可能會報錯
 
-# insert many
 function getRandomValue() {
   return Math.floor(Math.random() * 1000000);
 };
 
 let jsonArray = [];
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 1000000; i++) {
   let jsonObject = {
     "first_name": 'employee.first_name = ' + UUID(),
     "last_name": 'employee.last_name = ' + UUID(),
@@ -36,5 +38,7 @@ for (let i = 0; i < 100; i++) {
   jsonArray.push(jsonObject);
 }
 
-db.employee.insertMany(jsonArray);
+for (let i = 0; i < 100; i++) {
+  db.employee3.insertMany(jsonArray);
+}
 
